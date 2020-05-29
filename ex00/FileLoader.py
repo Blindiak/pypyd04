2a10e953517cb902beea155f0a18e1e5fd5ee83c
Blindiak <blindiak@gmail.com>
2020-05-30 00:48:47 +0200

    patch ex00 tail

--- ex00/FileLoader.py
+++ ex00/FileLoader.py
@@ -7,16 +7,17 @@ class FileLoader:
     def load(path):
         data = p.read_csv(path)
         s = data.shape
-        print("Loading dataset of dimensions" , s[0], "x", s[1] )
+        print("Loading dataset of dimensions", s[0], "x", s[1])
         return data
 
     @staticmethod
     def display(df, n):
-        print(df.head(n))
-
-
+        if n >= 0:
+            print(df.head(n))
+        else:
+            print(df.tail(-n))
 
 if __name__ == "__main__":
     loader = FileLoader()
-    data = loader.load("athlete_events.csv")
-    loader.display(data, 12)
+    data = loader.load("../athlete_events.csv")
+    loader.display(data, -12)
